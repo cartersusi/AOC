f25d4551f51e1745c48fb6c2495bd4a9eceb7994
Carter Susi <carter4299@gmail.com>
2024-12-01 19:53:34 -0600

    first commit

---
+++ 01/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func FindN(a int, arr []int) int {
+	res := 0
+	for _, val := range arr {
+		if val == a {
+			res++
+		}
+		if val > a {
+			return res
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fpath := "input"
+
+	file, err := os.Open(fpath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	var left []int
+	var right []int
+	for sc.Scan() {
+		line := sc.Text()
+		numbers := strings.Split(line, "   ")
+		left_n, err := strconv.ParseInt(numbers[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		right_n, err := strconv.ParseInt(numbers[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		left = append(left, int(left_n))
+		right = append(right, int(right_n))
+	}
+
+	sort.Ints(left)
+	sort.Ints(right)
+
+	res := 0
+	for i := range left {
+		//res += Abs(left[i] - right[i])
+		res += left[i] * FindN(left[i], right)
+	}
+
+	fmt.Println(res)
+
+}
---
+++ rand_lang.py
@@ -0,0 +1,31 @@
+import random
+import datetime
+
+languages = [
+    "Python",
+    "Java",
+    "C",
+    "C++",
+    "C#",
+    "JavaScript",
+    "Ruby",
+    "Perl",
+    "PHP",
+    #"Go",
+    "Swift",
+    "Objective-C",
+    "Rust",
+    "TypeScript",
+    "Lua",
+    "Haskell",
+    "Zig",
+    "Julia",
+    "R",
+    "Elixir",
+    "Clojure",
+    "Erlang",
+    "Julia",
+    "Lisp",
+    "OCaml"
+]
+print(f"{datetime.datetime.now().strftime("%m-%d")} | Language: {random.choice(languages)}")
