e058802d1fa6c3196488f52e3e5aef7b8847cec3
Carter Susi <carter4299@gmail.com>
2024-12-08 17:13:56 -0600

    update

--- 08/main.lua
+++ 08/main.lua
@@ -10,6 +10,7 @@ local fname = inputfiles[args[1]+1]
 function FileExists(file)
     local f = io.open(file, "rb")
     if f then f:close() end
+
     return f ~= nil
 end
 
@@ -18,39 +19,107 @@ function ReadFile(fpath)
     for line in io.lines(fpath) do
         res[#res + 1] = line
     end
+
     return res
 end
 
-function PrintTable(x, head)
-    local k = 5 if not head then k = #x end
+function PrintTable(t, head)
+    local k = 5 if not head then k = #t end
     for i = 1, k do
-        print(x[i])
+        print(t[i])
     end
 end
 
-function NearestNeighbor(q, x)
-    local min = math.huge
-    local res = nil
-
-    for _, v in x do
-        if v == q then goto continue end
-        local dx = q[1] - v[1]
-        local dy = q[2] - v[2]
-        local d = dx*dx + dy*dy
-        if d < min then
-            min = d
-            res = v
+function PrintAntennas(m)
+    for k, v in pairs(m) do
+        for i = 1, #v do
+            print(k, v[i][1], v[i][2])
         end
-        ::continue::
+    end
+end
+
+function Antinodes(t, x, d)
+    local da = {x[1] + d[1], x[2] + d[2]}
+    if da[1] < 1 or da[1] > #t then return t end
+
+    local ch = t[da[1]]:sub(da[2], da[2])
+    if ch ~= "." then return t end
+    print("Antinode: ", ch, da[1], da[2])
+
+    -- wtf
+    t[da[1]] = t[da[1]]:sub(1, da[2] - 1) .. "#" .. t[da[1]]:sub(da[2] + 1)
+
+    return t
+end
+
+function Frequencies(t, c, l)
+    for i = 1, #t do
+        local r = t[i]
+        for j = 1, #r do
+            local ch = r:sub(j, j)
+            local loc = {i, j}
+            if ch ~= c then goto continue end
+            if loc[1] == l[1] and loc[2] == l[2] then goto continue end
+
+            local d = {l[1] - i, l[2] - j}
+            -- Apply Diff to l
+            t = Antinodes(t, l, d)
+            -- Apply Diff to loc
+            d = {-d[1], -d[2]}
+            t = Antinodes(t, loc, d)
+
+            ::continue::
+        end
     end
 
-    return res
+    return t
+end
+
+function CountMat(t, ch)
+    local count = 0
+    for i = 1, #t do
+        local r = t[i]
+        for j = 1, #r do
+            local c = r:sub(j, j)
+            if c == ch then count = count + 1 end
+        end
+    end
+
+    return count
 end
 
 assert(fname ~= nil, "No input file specified")
 print("Using input file: " .. fname)
 
 assert(FileExists(fname))
-local x = ReadFile(fname)
-assert(#x > 0)
-PrintTable(x , true)
+local t = ReadFile(fname)
+assert(#t > 0)
+PrintTable(t , true)
+
+local antennas = {}
+for i = 1, #t do
+    local r = t[i]
+    for j = 1, #r do
+        local ch = r:sub(j, j)
+        if ch == "." then goto continue end
+        
+        if antennas[ch] == nil then antennas[ch] = {} end
+        local loc = {i, j}
+        antennas[ch][#antennas[ch] + 1] = loc
+        ::continue::
+    end
+end
+
+PrintAntennas(antennas)
+
+for k, v in pairs(antennas) do
+    print("Antenna: ", k)
+    for i = 1, #v do
+        local loc = v[i]
+        t = Frequencies(t, k, loc)
+        print()
+    end
+end
+
+PrintTable(t, false)
+print("Count: ", CountMat(t, "#"))
